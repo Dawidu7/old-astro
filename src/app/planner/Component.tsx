ad3b0bbed524009b95b366fd897cd5622d9ee676
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2023-08-27 17:21:27 +0200

    Planner: Edit & Delete

--- src/app/planner/Component.tsx
+++ src/app/planner/Component.tsx
@@ -2,16 +2,9 @@
 
 import clsx from "clsx"
 import { useState } from "react"
+import { FiEdit, FiTrash2 } from "react-icons/fi"
 import type { Options } from "./page"
-import {
-  Box,
-  Button,
-  Form,
-  Input,
-  Modal,
-  SearchSelect,
-  Separator,
-} from "~/components"
+import { Box, Button, Form, Input, Modal, SearchSelect } from "~/components"
 
 export default function Component({ options }: { options: Options }) {
   const [rows, setRows] = useState<Record<string, unknown>[]>([])
@@ -20,7 +13,11 @@ export default function Component({ options }: { options: Options }) {
     setRows(prev => [...prev, formData])
   }
 
-  console.log(rows)
+  async function editRow({ i, ...formData }: Record<string, unknown>) {
+    setRows(prev =>
+      prev.map((value, j) => (Number(i) === j ? formData : value)),
+    )
+  }
 
   return (
     <div className="relative mx-2 w-full max-w-7xl">
@@ -97,7 +94,10 @@ export default function Component({ options }: { options: Options }) {
           </thead>
           <tbody>
             {rows.map((row, i) => (
-              <tr className={clsx(i % 2 === 0 && "bg-zinc-800")} key={i}>
+              <tr
+                className={clsx("relative", i % 2 === 0 && "bg-zinc-800")}
+                key={i}
+              >
                 <td>{(row.catalog as { name: string }).name}</td>
                 <td>{row.number as string}</td>
                 <td>{(row.constellation as { name: string }).name}</td>
@@ -108,6 +108,98 @@ export default function Component({ options }: { options: Options }) {
                 <td>DEC {row.dec as string}</td>
                 <td>{row.info as string}</td>
                 <td>{(row.angle as { name: string }).name}°</td>
+                <td className="absolute right-0 top-[15%] flex gap-1">
+                  <Modal
+                    title="Edit Row"
+                    trigger={
+                      <Button
+                        className="text-zinc-400 data-[hovered]:text-white"
+                        plain
+                      >
+                        <FiEdit />
+                      </Button>
+                    }
+                  >
+                    {({ onClose }) => (
+                      <Form action={editRow} className="w-80">
+                        <input type="hidden" readOnly value={i} name="i" />
+                        <SearchSelect
+                          label="Catalog"
+                          options={options.catalog}
+                          name="catalog"
+                          defaultValue={row.catalog as { name: string }}
+                        />
+                        <Input
+                          label="Number"
+                          name="number"
+                          defaultValue={row.number as string}
+                        />
+                        <SearchSelect
+                          label="Constellation"
+                          options={options.constellation}
+                          name="constellation"
+                          defaultValue={row.constellation as { name: string }}
+                        />
+                        <SearchSelect
+                          label="Telescope"
+                          options={options.telescope}
+                          name="telescope"
+                          defaultValue={row.telescope as { name: string }}
+                        />
+                        <SearchSelect
+                          label="Camera"
+                          options={options.camera}
+                          name="camera"
+                          defaultValue={row.camera as { name: string }}
+                        />
+                        <SearchSelect
+                          label="Filter"
+                          options={options.filter}
+                          name="filter"
+                          defaultValue={row.filter as { name: string }}
+                        />
+                        <Input
+                          label="RA"
+                          name="ra"
+                          defaultValue={row.ra as string}
+                        />
+                        <Input
+                          label="DEC"
+                          name="dec"
+                          defaultValue={row.dec as string}
+                        />
+                        <Input
+                          label="Info"
+                          name="info"
+                          defaultValue={row.info as string}
+                        />
+                        <SearchSelect
+                          label="Angle"
+                          // @ts-expect-error
+                          options={options.angle.map(angle => ({
+                            ...angle,
+                            name: Number(angle.name),
+                          }))}
+                          name="angle"
+                          defaultValue={row.angle as { name: string }}
+                        />
+                        <Button type="submit" onPress={onClose}>
+                          Edit Row
+                        </Button>
+                      </Form>
+                    )}
+                  </Modal>
+
+                  <Button
+                    className="text-zinc-400 data-[hovered]:text-white"
+                    plain
+                    onPress={() =>
+                      setRows(prev => prev.filter((_, j) => i !== j))
+                    }
+                  >
+                    <FiTrash2 />
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
